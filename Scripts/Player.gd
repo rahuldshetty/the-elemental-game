ac8d0905615d591e12a6e842294e424075076581
Rahul <35rahuldshetty@gmail.com>
2023-05-27 22:00:17 +0530

    added element fusion and spells framework

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -22,15 +22,78 @@ var notifications = []
 @onready var player_notif_timer:Timer = $PlayerHUD/PlayerNotification/PlayerNotificationTimer
 
 # Spells
+@onready var orb1:TextureRect = $GamicHUD/Orb1
+@onready var orb2:TextureRect = $GamicHUD/Orb2
+@onready var orb3:TextureRect = $GamicHUD/Orb3
+
+@onready var spell1:TextureRect = $PlayerHUD/Control/SpellHud1/Spell1
+@onready var spell2:TextureRect = $PlayerHUD/Control/SpellHud2/Spell2
+
+var selected_orbs:Array = []
+const MAX_ORBS = 3
+
+var ORBS = [
+	{
+		"id": 0,
+		"name": "Fire",
+		"x": 0,
+	},
+	{
+		"id": 1,
+		"name": "Earth",
+		"x": 68,
+	},
+	{
+		"id": 2,
+		"name": "Wind",
+		"x": 100,
+	},
+	{
+		"id": 3,
+		"name": "Water",
+		"x": 32,
+	},
+	{
+		"id": 4,
+		"name": "Void",
+		"x": 128,
+	}
+]
+
+const SPELL_ICON_PREFIX = "res://Sprites/spell_gui/"
+
+var select_spells = []
+const MAX_SPELS = 2
+
+var SPELLS = {
+	Vector3i(0,0,0) : {
+		"id": 0,
+		"name": "Fire Blast",
+		"icon": "Spells/fire_spell.png",
+		"orbs": [0,0,0],
+		"function": fire_blast,
+		"mana": 30
+	}
+}
+
 var casting_spell = false
 
 ## Fire Blast
 @export var Fire_Blast_Scene: PackedScene
 const BLAST_OFF_DISTANCE:int = 150
-const FIRE_BLAST_MANA:int = 30
 
 func _ready():
 	animation.play("idle")
+	$GamicHUD.visible = true
+	
+	
+	orb1.visible = false
+	orb2.visible = false
+	orb3.visible = false
+	
+	spell1.visible =  false
+	spell2.visible = false
+	
 	hud.visible = true
 	init_player_stats()
 
@@ -102,21 +165,90 @@ func _on_player_notification_timer_timeout():
 		player_notif_timer.stop()
 		player_notification.visible = false
 	player_notification.text = "\n".join(notifications)
-	
+
+# Orbs Stuff
+func get_orb_draw_region(x):
+	return Rect2(x, 580, 32, 28)
+
+func draw_orbs():
+	if len(selected_orbs) >= 1:
+		orb1.texture.region = get_orb_draw_region(selected_orbs[0]['x'])
+		orb1.visible = true
+	if len(selected_orbs) >= 2:
+		orb2.texture.region = get_orb_draw_region(selected_orbs[1]['x'])
+		orb2.visible = true
+	if len(selected_orbs) >= 3:
+		orb3.texture.region = get_orb_draw_region(selected_orbs[2]['x'])
+		orb3.visible = true
+
+func add_orb(idx):
+	selected_orbs.push_front(ORBS[idx])
+	selected_orbs = selected_orbs.slice(0, MAX_ORBS)
+	draw_orbs()
+
+# Elemnet Fusion
+
+func draw_spells():
+	if len(select_spells) >= 1:
+		spell1.texture = load(SPELL_ICON_PREFIX + select_spells[0]['icon'])
+		spell1.visible = true
+	if len(select_spells) >= 2:
+		spell2.texture = load(SPELL_ICON_PREFIX + select_spells[1]['icon'])
+		spell2.visible = true
+
+func add_spell(spell):
+	if spell in select_spells:
+		return
+	select_spells.push_front(spell)
+	select_spells = select_spells.slice(0, MAX_SPELS)
+	draw_spells()
+
+func fuse_element():
+	if len(selected_orbs) == 3:
+		var key = Vector3i(
+			selected_orbs[0]['id'],
+			selected_orbs[1]['id'],
+			selected_orbs[2]['id']
+		)
+		if key in SPELLS:
+			add_spell(SPELLS[key])
+
 # Spells
 
 func _input(event):
-	if Input.is_action_just_pressed("shoot"):
-		fire_blast()
+	if Input.is_action_just_pressed("fuse_element"):
+		fuse_element()
+	if Input.is_action_just_pressed("cast_spell"):
+		cast_spell(0)
+	if Input.is_action_just_pressed("case_spell_2"):
+		cast_spell(1)
+	if Input.is_action_just_pressed("orb1"):
+		add_orb(0)
+	if Input.is_action_just_pressed("orb2"):
+		add_orb(1)
+	if Input.is_action_just_pressed("orb3"):
+		add_orb(2)
+	if Input.is_action_just_pressed("orb4"):
+		add_orb(3)
+	if Input.is_action_just_pressed("orb5"):
+		add_orb(4)
+	
+
+func cast_spell(idx):
+	if len(select_spells) >= idx + 1:
+		var mana = select_spells[idx]['mana']
+		if player_mana >= mana:
+			update_mana(-mana)
+			(select_spells[idx]['function']).call()
+
 		
 func cast_spell_animation():
 	animation.play("spell")
 	casting_spell = true
-		
+
 func fire_blast():
-	if Fire_Blast_Scene and player_mana >= FIRE_BLAST_MANA:
+	if Fire_Blast_Scene:
 		cast_spell_animation()
-		update_mana(-FIRE_BLAST_MANA)
 		var blast:Area2D = Fire_Blast_Scene.instantiate()
 		blast.position = global_position
 		if is_player_facing_left():
@@ -131,3 +263,5 @@ func _on_animated_sprite_2d_animation_finished():
 	if casting_spell:
 		casting_spell = false
 		animation.play("idle")
+
+
