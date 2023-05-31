b9a3bf4b386fd68eaad77ebf4a6204a544165838
Rahul <35rahuldshetty@gmail.com>
2023-05-31 20:31:45 +0530

    added better game menu ui

--- Scripts/menu/main_menu.gd
+++ Scripts/menu/main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+var show_controls = false
+
 func change_scene(new_scene):
 	get_tree().change_scene_to_file(new_scene)
+
+func _ready():
+	$Buttons/ControlMenu.visible = false
 
 func _on_play_button_pressed():
 	pass # Replace with function body.
@@ -17,3 +22,8 @@ func _on_settings_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_control_pressed():
+	show_controls = not show_controls
+	$Buttons/ControlMenu.visible = show_controls
